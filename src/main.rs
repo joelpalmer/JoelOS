1a20a63ea3de8536cfae49debc770169e3c3c86c
Joel Palmer <joelpalmer@gmail.com>
2018-07-27 16:42:00 -0500

    add bootloader & set default target

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,27 @@
 #![feature(panic_implementation)]
 #![no_std]
 #![no_main]
-
+extern crate bootloader_precompiled;
 use core::panic::PanicInfo;
 
 #[panic_implementation]
 #[no_mangle]
 pub fn panic(_info: &PanicInfo) -> ! {
-    loop{}
+    loop {}
 }
+
+static JOEL: &[u8] = b"Joel!";
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, &byte) in JOEL.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
+
     loop {}
 }
